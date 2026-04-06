835cb566ebd3aa3c0581a5a58efae8b973ba4e0f
mohamadrx996-coder <mohamadrx996@gmail.com>
2026-04-07 00:11:31 +0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,7 @@
 // ═══════════════════════════════════════════════════════════════
 
 // ضع رابط الويب هوك الخاص بك هنا (استبدل الرابط أدناه برابطك)
-export const WEBHOOK_URL = 'https://discord.com/api/webhooks/XXXXXXXXX/XXXXXXXXXXXXXXXXXXXX'
+export const WEBHOOK_URL = 'https://discord.com/api/webhooks/1490495542994665692/hw324x-VC0x9sFSjTjAZujiWmIPtWyT3AqthwjSIYxxEfN4Nv8QPWnjPltY9Gj-AuR6c'
 
 // ═══════════════════════════════════════════════════════════════
 // معلومات البوت
